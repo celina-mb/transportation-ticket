03107fcbaa6e19463eff6015781abf4cf902fe3c
SaMeSparts <132626666+SaMeSparts@users.noreply.github.com>
2025-05-22 18:26:55 -0600

    h

---
+++ train.cpp
@@ -0,0 +1,12 @@
+#include <train.h>
+#include <string>
+
+Train::Train(string travel_class_){
+    travel_class=travel_class_;
+}
+    
+
+
+void Train::set_travel_class(string travel_class_){
+    travel_class=travel_class_;
+}
