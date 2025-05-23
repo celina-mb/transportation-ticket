835a30a67f7f6d927b087945bab782b751466d57
SaMeSparts <132626666+SaMeSparts@users.noreply.github.com>
2025-05-22 18:37:36 -0600

    h

--- train.cpp
+++ train.cpp
@@ -1,11 +1,11 @@
-#include <train.h>
-#include <string>
+#include "Train.h"
 
-Train::Train(string travel_class_){
-    travel_class=travel_class_;
+Train::Train(string travel_class, int ticketID, float basePrice): Ticket(ticketID,basePrice){   
 }
     
-
+string Train::get_travel_class(){
+    return travel_class;
+}
 
 void Train::set_travel_class(string travel_class_){
     travel_class=travel_class_;
