8ae08004e9ed184fcc8e829502d0578029921934
iRodama05 <iker.rdz.amaro@gmail.com>
2025-05-22 19:49:27 -0600

    Update train.cpp

--- train.cpp
+++ train.cpp
@@ -1,18 +1,18 @@
 #include "Train.h"
 
-Train::Train(int id, double price, std::string tClass)
+TrainTicket::TrainTicket(int id, double price, std::string tClass)
     : Ticket(id, price), travelClass(tClass) {}
 
-float Train::calculatePrice() {
+double TrainTicket::calculatePrice() const {
     if (travelClass == "executive")
         return basePrice * 1.3;
     return basePrice;
 }
 
-std::string Train::getTravelClass() {
+std::string TrainTicket::getTravelClass() const {
     return travelClass;
 }
 
-void Train::setTravelClass(const std::string& tClass) {
+void TrainTicket::setTravelClass(const std::string& tClass) {
     travelClass = tClass;
 }
